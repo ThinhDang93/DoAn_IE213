c43a648e6c29f6cfc60e1b82c1188c28acc24e1f
ThinhDang93 <thinhdang0903@gmail.com>
2026-07-03 19:18:46 +0700

    Update interceptor.jsx

--- src/utils/interceptor.jsx
+++ src/utils/interceptor.jsx
@@ -5,6 +5,9 @@ import { getCookie, setCookie } from "./cookie";
 
 export const TOKEN = "accessToken";
 
+const DOMAIN = "https://movienew.cybersoft.edu.vn";
+const TOKEN_CYBERSOFT =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJEZXZPcHMgMDIiLCJIZXRIYW5TdHJpbmciOiIyOC8wOS8yMDI2IiwiSGV0SGFuVGltZSI6IjE3OTA1NTM2MDAwMDAiLCJuYmYiOjE3NjkwMTQ4MDAsImV4cCI6MTc5MDcwMTIwMH0.zSBJtqDLuPvKwr_on1Oi5AZu5DlaEWUjNmyd-Rbmy7Q";
 
 const token = localStorage.getItem(TOKEN);
 
